66d878bdb15a700f294b524ab5bb65825aa56609
sugamkuber <>
2023-02-27 18:39:42 +0530

    feat: adding client n server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
+const URL = "http://127.0.0.1:8080";
+
 function App() {
+  const Downloader = () => {
+    chrome.tabs.query(
+      { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+      (tabs) => {
+        const url = tabs[0].url;
+        const downloadUrl = `${URL}/download_chrome_ex?videoUrl=${url}`;
+        chrome.tabs.create({ url: downloadUrl });
+      }
+    );
+  };
   return (
     <>
-      <div>
-        You Tube Dowonloader
+      <div>You Tube Dowonloader</div>
+      <div className="flex justify-center">
+        <button onClick={Downloader}>Download</button>
       </div>
     </>
   );
